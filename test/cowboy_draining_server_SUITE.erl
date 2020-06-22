294f8a17c1ca35a4a59a607ea205ecee127515e5
Roman Pushkov <pushkovroman@me.com>
2020-06-22 17:54:37 +0300

    refactor tests

--- test/cowboy_draining_server_SUITE.erl
+++ test/cowboy_draining_server_SUITE.erl
@@ -34,31 +34,28 @@ init_per_suite(C) ->
     config().
 init_per_testcase(shutdown_test, C) ->
     genlib_app:start_application(ranch),
-    dummy_sup:start_link(2000),
-    unlink(whereis(dummy_sup)),
+    ok = dummy_sup:start(2000),
     C;
 init_per_testcase(request_interrupt_test, C) ->
     genlib_app:start_application(ranch),
-    dummy_sup:start_link(20000),
-    unlink(whereis(dummy_sup)),
+    ok = dummy_sup:start(20000),
     C.
 
 -spec end_per_testcase(test_case_name(), config()) ->
     ok.
 end_per_testcase(_Name, _C) ->
-    application:stop(ranch),
+    ok = application:stop(ranch),
     ok.
 
 -spec end_per_suite(config()) ->
     ok.
 end_per_suite(_C) ->
-    application:stop(hackney),
-    ok.
+    ok = application:stop(hackney).
 
 -spec shutdown_test(config()) ->
     ok.
 shutdown_test(_C) ->
-    Address = get_address(),
+    Address = dummy_sup:get_address(),
     ok = spawn_workers(Address, self(), ?NUMBER_OF_WORKERS),
     ok = timer:sleep(1000),
     ok = dummy_sup:stop(),
@@ -73,7 +70,7 @@ shutdown_test(_C) ->
 -spec request_interrupt_test(config()) ->
     ok.
 request_interrupt_test(_C) ->
-    Address = get_address(),
+    Address = dummy_sup:get_address(),
     ok = spawn_workers(Address, self(), ?NUMBER_OF_WORKERS),
     ok = timer:sleep(1000),
     ok = dummy_sup:stop(),
@@ -86,10 +83,6 @@ request_interrupt_test(_C) ->
                       timer:seconds(20)).
 
 %
-
-get_address() ->
-    {IP, Port} = ranch:get_addr(dummy_sup),
-    inet:ntoa(IP) ++ ":" ++ integer_to_list(Port).
 
 receive_loop(_MatchFun, N, _Timeout) when N =< 0 ->
     ok;
--- test/dummy_sup.erl
+++ test/dummy_sup.erl
@@ -2,23 +2,39 @@
 
 -behaviour(supervisor).
 
--export([start_link/1]).
+-export([start/1]).
 -export([stop/0]).
+-export([get_address/0]).
+
+-export([start_link/1]).
 
 -export([init/1]).
 
 -define(DEFAULT_ACCEPTORS_POOLSIZE, 100).
 
--spec start_link(Delay :: pos_integer()) ->
-    {ok, pid()} | {error, {already_started, pid()}}.
-start_link(Delay) ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, [Delay]).
+-spec start(Delay :: pos_integer()) ->
+    ok.
+start(Delay) ->
+    dummy_sup:start_link(Delay),
+    unlink(whereis(dummy_sup)),
+    ok.
 
 -spec stop() ->
     ok.
 stop() ->
     exit(whereis(?MODULE), kill),
     ok.
+
+-spec get_address() ->
+    string().
+get_address() ->
+    {IP, Port} = ranch:get_addr(?MODULE),
+    inet:ntoa(IP) ++ ":" ++ integer_to_list(Port).
+
+-spec start_link(Delay :: pos_integer()) ->
+    {ok, pid()} | {error, {already_started, pid()}}.
+start_link(Delay) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, [Delay]).
 
 -spec init([Delay :: pos_integer()]) ->
     {ok, {supervisor:sup_flags(), [supervisor:child_spec()]}}.
